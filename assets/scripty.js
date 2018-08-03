2e7747b7bf265d0d3dd741125effc4564ddbe6e9
Porter Wilcox <wilcox.porter@gmail.com>
2018-08-03 14:27:14 -0600

    added-select-dropdown

---
+++ assets/scripty.js
@@ -0,0 +1,5 @@
+let charArr = [
+    '<i class="fas fa-user-ninja char-icon ninja"></i>',
+    '<i class="fas fa-user-secret char-icon spy"></i>'
+];
+let charSelect = document.getElementById('character-selection');
